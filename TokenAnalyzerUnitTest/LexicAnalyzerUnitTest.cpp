9eb35662eba1130c449f5654187161a651fdec7e
Jasuse <61157367+Jasuse@users.noreply.github.com>
2021-01-21 17:47:59 +0300

    Unit Test: Unit Test now uses std::wstring instead of std::string Added method RunTest
    Known Issues:
    The way unit test finds root catalog is weird(need refactor!!!)
    Unit test doesn`t accept list of valid tokens(just for now)

--- TokenAnalyzerUnitTest/LexicAnalyzerUnitTest.cpp
+++ TokenAnalyzerUnitTest/LexicAnalyzerUnitTest.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 #include <sstream>
 #include <string>
-
+#include <algorithm>
 #include "..\Compiler\LexicAnalyzer.cpp"
 #include "..\Compiler\LexicAnalyzer.h"
 #include "..\Compiler\Token.h"
@@ -15,27 +15,32 @@ namespace LexicAnalyzerUnitTest {
 
 TEST_CLASS(LexicAnalyzerUnitTest) {
  public:
-  const std::string tests_directory = "lexic_analyzer_tests_";
+  const std::wstring tests_directory = L"lexic_analyzer_tests_";
+  const std::wstring project_name = L"Compiler";
+  void RunTest(std::wstring input_filename, std::wstring expected_filename)
+  {
+      std::wstring currentPath = std::filesystem::current_path().wstring();
+      size_t project_name_offset = currentPath.find(project_name);
+      size_t first_backslash = currentPath.find_first_of('\\', project_name_offset);
+      currentPath = currentPath.substr(0, first_backslash + 1) + tests_directory + L"\\";
+      std::ifstream file_input(currentPath + input_filename);
+      LexicAnalyzer analyzer;
+      std::queue<Token> actual_tokens = analyzer.GetTokens(file_input);
+      std::ifstream file_expected(currentPath + expected_filename);
+      Assert::IsTrue(file_input.is_open() && file_expected.is_open(),
+                     L"UNABLE TO OPEN INPUT OR/AND EXPECTED FILE(S)\nCHECK IF THEY ARE IN TESTS DIRECTORY");
+      std::string line;
+      while (std::getline(file_expected, line)) {
+          Assert::IsTrue(!actual_tokens.empty(), L"QUEUE IS EMPTY");
+          Token actual_token = actual_tokens.front();
+          Assert::IsTrue(line.compare(std::string(std::to_string((int)actual_token.type)) +
+                         " " + actual_token.symbol), L"TOKENS DO NOT MATCH");
+          actual_tokens.pop();
+      }
+  }
+
   TEST_METHOD(Test1) {
-    std::string currentPath = std::filesystem::current_path().string();
-    if (currentPath.find("Debug") != std::string::npos) {
-      currentPath = currentPath.substr(0, currentPath.find("Debug"));
-    } 
-    currentPath += "\\" + tests_directory + "\\";
-    std::ifstream file_input(currentPath + "1_input.txt");
-    LexicAnalyzer analyzer;
-    std::queue<Token> actual_tokens = analyzer.GetTokens(file_input);
-    std::ifstream file_expected(currentPath + "1_expected.txt");
-    Assert::IsTrue(file_input.is_open() && file_expected.is_open(),
-                   std::filesystem::current_path().c_str());
-    std::string line;
-    while (std::getline(file_expected, line)) {
-      Assert::IsTrue(!actual_tokens.empty(), L"QUEUE IS EMPTY");
-      Token actual_token = actual_tokens.front();
-      Assert::IsTrue(line.compare(std::string(std::to_string((int)actual_token.type)) +
-                       " " + actual_token.symbol), L"TOKENS DO NOT MATCH");
-      actual_tokens.pop();
-    }
+    RunTest(L"1_input.txt", L"1_expected.txt");
   }
 };
 
